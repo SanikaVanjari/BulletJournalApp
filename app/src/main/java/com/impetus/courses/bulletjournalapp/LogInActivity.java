1b5db49f3d18295f6376c5b091ac04c54aa95ddd
Sanika Vanjari <sanikavanjari94@gmail.com>
2019-01-09 20:54:27 +0530

    LogIn activity,registration activity and resend verification mail activity complete

--- app/src/main/java/com/impetus/courses/bulletjournalapp/LogInActivity.java
+++ app/src/main/java/com/impetus/courses/bulletjournalapp/LogInActivity.java
@@ -1,22 +1,38 @@
 package com.impetus.courses.bulletjournalapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
     Button logIn;
     EditText email,password;
     TextView register,forgot_password,resend_mail;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressBar mProgressBar;
+
+    private static final String TAG="LogInActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
         //initiating the xml
         logIn=findViewById(R.id.button);
         email=findViewById(R.id.editText);
@@ -31,14 +47,42 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         forgot_password.setOnClickListener(this);
         resend_mail.setOnClickListener(this);
 
+        mProgressBar =  findViewById(R.id.progressBar);
 
-    }
+        setupFirebaseAuth();
+
+
+        }
 
     @Override
     public void onClick(View v) {
         if(v== logIn){
-            Intent intent= new Intent(this,HomeActivity.class);
-            startActivity(intent);
+            if(!isEmpty(email.getText().toString())
+                    && !isEmpty(password.getText().toString())){
+                Log.d(TAG, "onClick: attempting to authenticate.");
+
+                showDialog();
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(),
+                        password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                hideDialog();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LogInActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        hideDialog();
+                    }
+                });
+            }else{
+                Toast.makeText(LogInActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
+            }
+            hideSoftKeyboard();
         }
         if(v== register){
             Intent intent= new Intent(this,RegisterActivity.class);
@@ -49,6 +93,81 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         }
         if(v== resend_mail){
             //code for resend mail popup
+            ResendVerificationDialog dialog = new ResendVerificationDialog();
+            dialog.show(getSupportFragmentManager(), "dialog_resend_email_verification");
+        }
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    /*
+        ----------------------------- Firebase setup ---------------------------------
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started.");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    //check if email is verified
+                    if(user.isEmailVerified()){
+                        Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                        Toast.makeText(LogInActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        Intent intent= new Intent(LogInActivity.this,HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }else{
+                        Toast.makeText(LogInActivity.this, "Email is not Verified\nCheck your Inbox", Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
         }
     }
 }
--- app/src/main/java/com/impetus/courses/bulletjournalapp/RegisterActivity.java
+++ app/src/main/java/com/impetus/courses/bulletjournalapp/RegisterActivity.java
@@ -1,14 +1,29 @@
 package com.impetus.courses.bulletjournalapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG="RegisterActivity";
     Button user_register;
     EditText user_name,user_phone,user_DOB,user_email,user_password,user_confirm_password;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         user_email=findViewById(R.id.userEmail);
         user_password=findViewById(R.id.userPassword);
         user_confirm_password=findViewById(R.id.userConfirmPassword);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        firebaseAuth=FirebaseAuth.getInstance();
 
         //onClick listeners
         user_register.setOnClickListener(this);
@@ -31,7 +49,134 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         if(v==user_register){
+            Log.d(TAG, "onClick: attempting to register.");
 
+            //check for null valued EditText fields
+            if(!isEmpty(user_email.getText().toString())
+                    && !isEmpty(user_password.getText().toString())
+                    && !isEmpty(user_confirm_password.getText().toString())){
+
+                    //check if passwords match
+                    if(doStringsMatch(user_password.getText().toString(), user_confirm_password.getText().toString())){
+
+                        //Initiate registration task
+                        registerNewEmail(user_email.getText().toString(), user_password.getText().toString());
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
+                    }
+
+
+            }else{
+                Toast.makeText(RegisterActivity.this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+            }
         }
+        hideSoftKeyboard();
+
+    }
+
+    /**
+     * Register a new email and password to Firebase Authentication
+     * @param email
+     * @param password
+     */
+    public void registerNewEmail(final String email, String password){
+
+        showDialog();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "onComplete: AuthState: " + FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                            //send email verificaiton
+                            sendVerificationEmail();
+
+                            FirebaseAuth.getInstance().signOut();
+
+                            //redirect the user to the login screen
+                            redirectLoginScreen();
+                        }
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Unable to Register",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        hideDialog();
+
+                        // ...
+                    }
+                });
+    }
+
+    /**
+     * sends an email verification link to the user
+     */
+    private void sendVerificationEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Sent Verification Email", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Couldn't Verification Send Email", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    /**
+     * Redirects the user to the login screen
+     */
+    private void redirectLoginScreen(){
+        Log.d(TAG, "redirectLoginScreen: redirecting to login screen.");
+
+        Intent intent = new Intent(RegisterActivity.this, LogInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Return true if @param 's1' matches @param 's2'
+     * @param s1
+     * @param s2
+     * @return
+     */
+    private boolean doStringsMatch(String s1, String s2){
+        return s1.equals(s2);
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
